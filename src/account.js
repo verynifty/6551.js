ee0c574f9de0aeede7061189f557c8df54440c48
grandsmarquis <jdourlens@gmail.com>
2023-04-10 17:40:09 +0900

    wip

--- src/account.js
+++ src/account.js
@@ -1,3 +1,6 @@
+const accountABI = require("../abis/ERC6551Account.json");
+const { ethers } = require("ethers");
+
 function ERC6551Account(instance, chainId, tokenContract, tokenId, implementationAddress, salt) {
     this.instance = instance;
     this.chainId = chainId;
@@ -6,6 +9,8 @@ function ERC6551Account(instance, chainId, tokenContract, tokenId, implementatio
     this.implementationAddress = implementationAddress;
     this.salt = salt;
     this.address = this.instance.registry.getAccountAddress(chainId, tokenContract, tokenId, implementationAddress, salt);
+    this.contract = new ethers.Contract(this.address, accountABI);
+    this.contract.attach(this.instance.provider);
 }
 
 ERC6551Account.prototype.isDeployed = async function() {
@@ -14,6 +19,18 @@ ERC6551Account.prototype.isDeployed = async function() {
 
 ERC6551Account.prototype.deploy = async function() {
     // make tx to create account
+}
+
+ERC6551Account.prototype.executeCall = async function() {
+    // make tx to account
+}
+
+ERC6551Account.prototype.nonce = async function() {
+    // get nonce
+}
+
+ERC6551Account.prototype.owner = async function() {
+    // get owner
 }
 
 module.exports = ERC6551Account;
--- src/index.js
+++ src/index.js
@@ -11,5 +11,15 @@ ERC6551.prototype.getAccount = function(chainId, tokenContract, tokenId, impleme
     return new ERC6551Account(this, this.registry, chainId, tokenContract, tokenId, implementationAddress, salt)
 }
 
+ERC6551.prototype.doesAccountExists = async function(chainId, tokenContract, tokenId, implementationAddress, salt) {
+    const account = new ERC6551Account(this, this.registry, chainId, tokenContract, tokenId, implementationAddress, salt)
+    return await account.isDeployed()
+}
+
+// a helper to encapsulate params to be used p = makeParams(a, b, c...) => getAccount(...p)
+ERC6551.prototype.makeParams = function(chainId, tokenContract, tokenId, implementationAddress, salt) {
+    return [chainId, tokenContract, tokenId, implementationAddress, salt];
+}
+
 module.exports = ERC6551;
 
